b7f82f76cbe310b6b4477c6cdea7c6d1f476ab93
v.kalmykov <v.kalmykov@tinkoff.ru>
2023-01-06 23:58:00 +0300

    add signal handling

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use prometheus_exporter_base::prelude::Authorization;
 use prometheus_exporter_base::prelude::ServerOptions;
 use prometheus_exporter_base::render_prometheus;
 use prometheus_wasd_exporter::{config, serve_metrics};
+use tokio::signal::unix::signal;
+use tokio::signal::unix::SignalKind;
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     color_eyre::install()?;
@@ -15,6 +17,14 @@ async fn main() -> eyre::Result<()> {
         authorization: Authorization::None,
     };
     let serve = |_, _| async move { Ok(serve_metrics(&channels.clone()).await) };
-    tokio::spawn(render_prometheus(server_options, (), serve)).await?;
+    let mut sighup = signal(SignalKind::hangup())?;
+    let mut sigint = signal(SignalKind::interrupt())?;
+    let mut sigterm = signal(SignalKind::terminate())?;
+    tokio::select! {
+        res = tokio::spawn(render_prometheus(server_options, (), serve)) => res?,
+        _ = sighup.recv() => todo!(),
+        _ = sigint.recv() => println!("recieved SIGINT, exiting..."),
+        _ = sigterm.recv() => println!("recieve SIGTERM, exiting...")
+    }
     Ok(())
 }
