3d203255987e98250935742c584608893d589f0a
BulbaWarrior <vladkalm49@gmail.com>
2023-06-24 19:50:06 +0300

    config error handling

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,16 @@
-use color_eyre::eyre;
+use color_eyre::eyre::{self, eyre};
 use std::fs::File;
 use std::io;
 
 fn read_conf_from(file_name: &str) -> eyre::Result<Vec<String>> {
-    let file = File::open(file_name)?;
-    let conf: String = io::read_to_string(file)?;
+    let file = match File::open(file_name) {
+        Ok(f) => f,
+        Err(e) => return Err(eyre!("could not open config file {file_name}: {e}")),
+    };
+    let conf: String = match io::read_to_string(file) {
+        Ok(s) => s,
+        Err(e) => return Err(eyre!("problem reading config: {e}")),
+    };
     let channels = conf.lines().map(|x| x.to_string()).collect();
     Ok(channels)
 }
